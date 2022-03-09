a207465b455de0250d0660dd6a617565092d030d
Larry S <muppethead@mac.com>
2022-03-09 19:27:17 +0000

    Initial commit

---
+++ TODO.md
@@ -0,0 +1,11 @@
+- popover text for teasers?
+https://codepen.io/chocochip/pen/zYxMgRG
+
+- Pagination
+https://betterprogramming.pub/simple-flask-pagination-example-4190b12c2e2e
+https://www.adamsmith.haus/python/answers/how-to-take-a-subset-of-a-dictionary-in-python
+
+- Add more content
+  
+- ~~Group by tag (e.g. "B-Lab", "One-pages", "I Am a Pineapple", etc.)~~
+- 
---
+++ main.py
@@ -0,0 +1,60 @@
+import os, json, markdown
+from flask import Flask, render_template
+
+app = Flask('app')
+
+@app.template_filter('md')
+def md(text):
+    return markdown.markdown(text)
+
+def getItems(path):
+    file = open(path)
+    data = file.read()
+    items = json.loads(data)
+    return items
+    
+CONTENT = 'comics.json'
+JSON_PATH = os.path.join(app.static_folder, CONTENT)
+ITEMS = getItems(JSON_PATH)
+
+@app.route('/')
+def home():
+    featured = ITEMS[1:9]
+    return render_template('home.jinja', items=ITEMS, featured=featured)
+    
+@app.route('/comics/')
+def archives():
+    return render_template('archives.jinja', items=ITEMS)
+
+@app.route('/comics/series/<seriesId>/')
+def category(seriesId):
+    items = [row for row in ITEMS if row['series'] == seriesId]
+    return render_template('series.jinja', items=items, seriesId=seriesId)
+
+@app.route('/comics/<slug>/')
+def detail(slug):
+    idx, item = [(index, row) for index, row in enumerate(ITEMS) if row['slug'] == slug][0]
+    
+    if idx < len(ITEMS) - 1:
+        next = ITEMS[idx + 1]
+    else:
+        next = ITEMS[0]
+
+    if idx > 0:
+        prev = ITEMS[idx - 1]
+    else:
+        prev = ITEMS[len(ITEMS) - 1]
+    
+    return render_template('detail.jinja', item=item, prev=prev, next=next)
+
+
+
+@app.route('/game/')
+def game():
+  return render_template('game.jinja')
+
+@app.route('/about/')
+def about():
+  return render_template('about.jinja')
+
+app.run(host='0.0.0.0', port=8080)

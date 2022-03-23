9243fcc1788c03d4854eee2686446d524361e8be
Larry S <muppethead@mac.com>
2022-03-23 22:58:28 +0000

    Added "best of"

--- main.py
+++ main.py
@@ -18,24 +18,24 @@ CONTENT = 'comics.json'
 JSON_PATH = os.path.join(app.static_folder, CONTENT)
 ITEMS = getItems(JSON_PATH)
 
+# HOME
 @app.route('/')
 def home():
-    return render_template('home.jinja', hero=ITEMS[0], items=ITEMS[1:13])
-    
+    bestList = [item for item in ITEMS if 'bestof' in item]
+    return render_template('home.jinja', items=bestList[0:12])
+
+# ALL COMICS SHUFFLED
 @app.route('/comics/')
 def archives():
     return render_template('archives.jinja', items=sample(ITEMS, len(ITEMS)) )
 
+# RANDOMIZER URL
 @app.route('/random/')
 def random():
     item=sample(ITEMS,1)[0]
     return redirect('/comics/' + item['slug'] + '/')
-    # return render_template('random.jinja', item=sample(ITEMS,1)[0])
 
-# @app.route('/comics/series/<seriesId>/')
-# def category(seriesId):
-#     return render_template('series.jinja', items=[row for row in ITEMS if row['series'] == seriesId], seriesId=seriesId)
-
+# DETAIL PAGE
 @app.route('/comics/<slug>/')
 def detail(slug):
     idx, item = [(index, row) for index, row in enumerate(ITEMS) if row['slug'] == slug][0]
@@ -45,21 +45,15 @@ def detail(slug):
     else:
         next = ITEMS[0]
 
-    if idx > 0:
-        prev = ITEMS[idx - 1]
-    else:
-        prev = ITEMS[len(ITEMS) - 1]
-    
-    return render_template('detail.jinja', item=item, prev=prev, next=next)
+    return render_template('detail.jinja', item=item, next=next)
 
-
-
-@app.route('/game/')
-def game():
-  return render_template('game.jinja')
-
+# ABOUT
 @app.route('/about/')
 def about():
   return render_template('about.jinja')
 
+# @app.route('/game/')
+# def game():
+#   return render_template('game.jinja')
+    
 app.run(host='0.0.0.0', port=8080)
